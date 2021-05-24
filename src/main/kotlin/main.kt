41bd6c6992a344b219bd22ea1fb06b172242d3e1
ryg-git <ygretharekar@outlook.com>
2021-05-24 23:05:49 +0530

    [BC 201 E] Xor Distances

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,34 +1,50 @@
-fun next() = readLine()!!
+fun next() = readLine()!!.trim()
 
+@kotlin.ExperimentalStdlibApi
 fun main() {
-    val (h, w) = readLine()!!.split(" ").map(String::toInt)
-    val board = mutableListOf<List<Int>>()
-    repeat(h) {
-        val row = readLine()!!.trim().toCharArray().map { c -> if(c == '+') 1 else -1 }.toList()
-        board.add(row)
+    val n = next().toInt()
+
+    val edges = List(n - 1) {
+        val (u, v, w) = next().split(" ")
+        Triple(u.toInt() - 1, v.toInt() - 1, w.toLong())
     }
-    val dp = Array(h) {IntArray(w) {0}}
-    val getTurn = { r: Int, c: Int -> (r + c) and 1 == 0 }
 
-    for (i in h - 1 downTo 0)
-        for (j in w - 1 downTo 0) {
-            if (i == h - 1 && j == w - 1) continue
-            if (getTurn(i, j)) {
-                dp[i][j] = Int.MIN_VALUE
-                if(j < w - 1) dp[i][j] = maxOf(dp[i][j], dp[i][j + 1] + board[i][j + 1])
-                if(i < h - 1) dp[i][j] = maxOf(dp[i][j], dp[i + 1][j] + board[i + 1][j])
-            } else {
-                dp[i][j] = Int.MAX_VALUE
-                if(j < w - 1) dp[i][j] = minOf(dp[i][j], dp[i][j + 1] - board[i][j + 1])
-                if(i < h - 1) dp[i][j] = minOf(dp[i][j], dp[i + 1][j] - board[i + 1][j])
-            }
+    val nodes = List(n){mutableListOf<Pair<Int, Long>>()}
+
+    edges.forEach { (u, v, w) ->
+        nodes[u].add(v to w)
+        nodes[v].add(u to w)
+    }
+
+    val fromRoot = Array(n) {-1L}.also { it[edges.first().first] = 0L }
+
+    val q = ArrayDeque<Int>()
+
+    q.addFirst(edges.first().first)
+
+    while (!q.isEmpty()) {
+        val u = q.removeLast()
+        for ((v, w) in nodes[u]) {
+            if(fromRoot[v] >= 0) continue
+            fromRoot[v] = fromRoot[u] xor w
+            q.addFirst(v)
         }
+    }
 
-    val ans = dp[0][0]
+    val cnt = MutableList(60) {0} to MutableList(60) {0}
 
-    when {
-        ans > 0 -> "Takahashi"
-        ans < 0 -> "Aoki"
-        else -> "Draw"
-    }.let(::println)
+    val mod = 1000_000_007
+
+    var ans = 0L
+
+    for (i in 0 until 60) {
+        for (d in fromRoot) {
+            if(d shr i and 1L == 0L) cnt.first[i]++
+            else cnt.second[i]++
+        }
+        ans += (cnt.first[i]*((1L shl i) % mod))%mod * cnt.second[i]
+        ans %= mod
+    }
+
+    ans.let(::println)
 }
